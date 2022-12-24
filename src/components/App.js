8844c4da162a8a0a147d49e8662fb0a4a560e0ec
jagtapnimisha2 <120504401+jagtapnimisha2@users.noreply.github.com>
2022-12-24 17:09:36 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,18 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  render(){
   return (
     <div id="main">
       // Do not alter the main div
+        <button id="click"  onclick="click()">click</button>
+    function click(){
+    <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+  }
+
     </div>
   );
 }
 
-
+}
 export default App;
