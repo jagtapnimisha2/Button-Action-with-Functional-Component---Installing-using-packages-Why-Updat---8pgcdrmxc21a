a98e36384eb553acb05ce5540140a6d902200708
jagtapnimisha2 <120504401+jagtapnimisha2@users.noreply.github.com>
2022-12-24 17:15:59 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -7,10 +7,11 @@ function App() {
     <div id="main">
       // Do not alter the main div
         <button id="click"  onclick="click()">click</button>
+//     kuch bhi
     function click(){
     <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
   }
-
+click();
     </div>
   );
 }
